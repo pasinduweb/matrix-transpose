5d5f84aa7ed202949457970f4229e1307e0dd71a
Pasindu Manaranjana Jayasekara <algopasindu@gmail.com>
2023-07-12 10:39:19 +0530

    removed codes come from main branch

--- matrixTranspose.js
+++ matrixTranspose.js
@@ -25,21 +25,11 @@ const transposedMatrix = transposeMatrix(matrix);
 for (let row of transposedMatrix) {
   console.log(row.join(' '));
 }
-// --- ALGORITHM END ---
 
-
-// --- Below Things for Testing Purposes ---
-
-// a function created to check git github branch and merging
 const localBranch = () => {
   console.log("Branch Created on Local Machine");
 };
 
-// a function created to check git github branch and merging
-const remoteBranch = () => {
-  console.log("Branch Created on Remote Server");
-};
-
 localBranch();
-remoteBranch();
+
 
